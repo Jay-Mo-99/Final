54756065c57eb0f87573611395dca9e2ac937f89
Jay Mo <jaym991776@gmail.com>
2022-12-13 11:43:50 -0500

    Read the text file to get the name of pins

--- exam.cpp
+++ exam.cpp
@@ -1,6 +1,19 @@
+#pragma warning(disable:4996)
 #include <stdio.h>
 
 int main(void)
 {
+	const int size = 21;
+	FILE* fp1;
+	char name[size];
+
+
+	//Open the file for get the pin name
+	fp1 = fopen("pinName.txt", "rt");
+	if (fp1 == NULL)
+	{
+		printf("Error : Can't open file.");
+		return 0;
+	}
 
 }
