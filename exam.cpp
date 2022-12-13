7dd62edc6318d26e4edc2fe29db63c759312b5b1
Jay Mo <jaym991776@gmail.com>
2022-12-13 12:08:29 -0500

    Read the pin number in the text file

--- exam.cpp
+++ exam.cpp
@@ -1,12 +1,24 @@
+/*
+* Filename: 		exam.cpp
+* Project:			exam.cpp
+* Programmer:		Jay Mo
+* Date:     		December 13, 2022
+* Description:  	This program uses File Input Option.
+
+*/
+
 #pragma warning(disable:4996)
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
 int main(void)
 {
-	const int size = 21;
+	const int size = 254;
 	FILE* fp1;
-	char name[size];
-
+	char name[21][size] = {" "};
+	int numRead = 0;
+	char* realname;
 
 	//Open the file for get the pin name
 	fp1 = fopen("pinName.txt", "rt");
@@ -15,5 +27,15 @@ int main(void)
 		printf("Error : Can't open file.");
 		return 0;
 	}
+
+
+
+	for(int i = 0;i < 21;i++)
+	{
+		fgets(name[i], sizeof(name[i]), fp1);
+		realname = strtok(name[i],"\n");
+		printf("The pin number : %s\n", name[i]);
+	}
+
 
 }
